590b31fa21082fd30cbba16994a317c7859632a9
Gregory Rodriguez <grodriguez13@myseneca.ca>
2023-06-04 22:40:38 -0400

    add reflect

--- workshops/ws3/diy/CalorieList.cpp
+++ workshops/ws3/diy/CalorieList.cpp
@@ -1,39 +1,9 @@
 /*
-+----------------------------------------------------+
-|  Daily Calorie Consumption                         |
-+--------------------------------+------+------------+
-| Food name                      | Cals | When       |
-+--------------------------------+------+------------+
-| Cheerios Cereal with 2% milk.. |  170 | Breakfast  |
-| Tim Hortons Medium coffee doub |  230 | Breakfast  |
-| Cheeseburger.................. |  303 | Lunch      |
-| French Fries.................. |  312 | Lunch      |
-| Pepsi......................... |  150 | Lunch      |
-| Apple......................... |   52 | Snack      |
-| Bread and Cheese.............. |  195 | Dinner     |
-| Garden Salad with Dressing.... |  220 | Dinner     |
-| Red Wine...................... |   85 | Dinner     |
-+--------------------------------+------+------------+
-|    Total Calories for today:     1717 |            |
-+----------------------------------------------------+
-class CalorieList {
-
-    char m_title[30];
-    Food* m_food;
-    int m_noOfFood;
-    int m_foodAdded;
-    int totalCalories;
-    void Title() const;
-    void footer() const;
-    void setEmpty();
-    bool isValid();
-
-    public:
-    void init(const char* title, int noOfFood);
-    bool add(const char* food_name, int calories, int timeOfConsumption);
-    void display() const;
-    void deallocate();
-};
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+Name: Gregory Rodriguez 
+E-mail: grodriguez13@myseneca.ca
+Student #: 127880227
+Date: June 4, 2023
 */
 
 #include "CalorieList.h"
--- workshops/ws3/diy/cstring.h
+++ workshops/ws3/diy/cstring.h
@@ -1,3 +1,10 @@
+/*
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+Name: Gregory Rodriguez 
+E-mail: grodriguez13@myseneca.ca
+Student #: 127880227
+Date: June 4, 2023
+*/
 #ifndef SDDS_CSTRING_H
 #define SDDS_CSTRING_H
 
