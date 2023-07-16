8693fc7edb7d98a1be3a10f08315fc600d91dcdb
Gregory Rodriguez <grodriguez13@myseneca.ca>
2023-07-15 22:10:53 -0400

    ms1 attempt 1

---
+++ workshops/project/Menu.cpp
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Menu.h"
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+namespace sdds {
+// Menu Class
+ostream& operator<<(ostream& ostr, const Menu& menu) {
+    ostr << menu.getMenuTitle();
+    return ostr;
+}
+
+const char* Menu::operator[](unsigned int index) const {
+    return (const char*)(m_menuItems[index]->m_itemName);
+}
+
+const char* Menu::getMenuTitle() const {
+    return m_menuTitle->m_itemName ? m_menuTitle->m_itemName : "";
+}
+
+Menu::operator bool() const {
+    return m_numOfItems;
+}
+
+Menu::operator int() const {
+    return m_numOfItems;
+}
+
+Menu::operator unsigned int() const {
+    return m_numOfItems;
+}
+
+Menu& Menu::operator<<(const char* menuItemContent) {
+    if (m_numOfItems < MAX_MENU_ITEMS) {
+        m_menuItems[m_numOfItems] = new MenuItem(menuItemContent);
+        m_numOfItems++;
+    }
+    return *this;
+}
+
+unsigned int Menu::operator~() const {
+    return run();
+}
+
+unsigned int Menu::run() const {
+    unsigned int selection = 1;
+    displayTitle();
+    displayMenu();
+    cout << "> ";
+    do {
+        cin.clear();
+        cin >> selection;
+        if (cin.fail() || selection > m_numOfItems) {
+            cout << "Invalid Selection, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return selection;
+        }
+    } while (true);
+}
+
+ostream& Menu::displayMenu() const {
+    for (int i = 0; i < m_numOfItems; i++) {
+        cout << setw(2) << right << i + 1 << "- " << m_menuItems[i]->m_itemName << endl;
+    }
+    cout << setw(4) << right << "0- "
+         << "Exit" << endl;
+    return cout;
+}
+
+ostream& Menu::displayTitle() const {
+    const char* title = getMenuTitle();
+    if (title[0] != '\0') {
+        cout << title << ":" << endl;
+    }
+    return cout;
+}
+
+Menu::Menu() {
+    m_menuTitle = new MenuItem();
+    m_numOfItems = 0;
+}
+
+Menu::Menu(const char* srcTitle) {
+    m_menuTitle = new MenuItem(srcTitle);
+    m_numOfItems = 0;
+}
+
+Menu::~Menu() {
+    for (int i = 0; i < MAX_MENU_ITEMS; i++) {
+        delete m_menuItems[i];
+    }
+    delete m_menuTitle;
+}
+
+// MenuItem Class
+MenuItem::MenuItem() {
+    m_itemName = nullptr;
+}
+
+MenuItem::MenuItem(const char* itemName) {
+    m_itemName = new char[strlen(itemName)];
+    strcpy(m_itemName, itemName);
+}
+
+MenuItem::~MenuItem() {
+    delete[] m_itemName;
+}
+
+MenuItem::operator bool() const {
+    return m_itemName;
+}
+
+MenuItem::operator const char*() const {
+    return m_itemName;
+}
+}  // namespace sdds
---
+++ workshops/project/Menu.h
@@ -0,0 +1,47 @@
+#ifndef SDDS_MENU_H__
+#define SDDS_MENU_H__
+#include <iostream>
+
+namespace sdds {
+const unsigned int MAX_MENU_ITEMS = 20;
+
+class MenuItem {
+   private:
+    friend class Menu;
+    char* m_itemName;
+    MenuItem();
+    MenuItem(const char* itemName);
+    MenuItem(const MenuItem&) = delete;
+    MenuItem& operator=(const MenuItem&) = delete;
+    virtual ~MenuItem();
+    operator bool() const;
+    operator const char*() const;
+};
+
+class Menu {
+   private:
+    MenuItem* m_menuTitle{};
+    MenuItem* m_menuItems[MAX_MENU_ITEMS]{};
+    unsigned int m_numOfItems;
+    Menu(const Menu&) = delete;
+    Menu& operator=(const Menu&) = delete;
+
+   public:
+    Menu();
+    Menu(const char* srcTitle);
+    virtual ~Menu();
+    unsigned int run() const;
+    std::ostream& displayMenu() const;
+    std::ostream& displayTitle() const;
+    const char* getMenuTitle() const;
+    Menu& operator<<(const char* menuItemContent);
+    const char* operator[](unsigned int index) const;
+    operator int() const;
+    operator unsigned int() const;
+    operator bool() const;
+    unsigned int operator~() const;
+};
+
+std::ostream& operator<<(std::ostream& ostr, const Menu& menu);
+}  // namespace sdds
+#endif
